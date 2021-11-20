5c4585c3e33cb573b35fc03bbe9e7272c9dbfb1a
Tyler Tian <tylertian123@gmail.com>
2021-11-20 04:06:20 -0500

    Add basic cross-section definition

--- calculate.py
+++ calculate.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import Any, Dict, List, Tuple
+from matplotlib import pyplot as plt, patches
 
 import yaml
 try:
@@ -15,7 +16,7 @@ Forces = List[Tuple[float, float]]
 
 # A class to hold all the bridge and loading geometry constants
 class Geometry:
-    def __init__(self, values: Dict[str, Any]):
+    def __init__(self, values: Dict[str, Any]) -> None:
         self.train_wheel_load = values["train"]["totalWeight"] / 3 / 2 # Load on each wheel
         self.train_wheel_distance = values["train"]["wheelDistance"] # Distance between two wheels on the same carriage
         self.train_wheel_edge_distance = values["train"]["wheelEdgeDistance"] # Distance between the wheel and edge of the carriage
@@ -24,10 +25,31 @@ class Geometry:
         self.bridge_length = values["bridge"]["length"] # Length of the entire bridge
         self.bridge_supports = values["bridge"]["supports"] # Location of all the supports
 
+        self.bridge_cross_sections = [CrossSection(d) for d in values["bridge"]["crossSections"]]
     @classmethod
     def from_yaml(cls, file: str) -> "Geometry":
         return Geometry(yaml.load(open(file, "r", encoding="utf-8"), Loader))
 
+
+# A class to hold and compute properties of cross sections
+class CrossSection:
+    def __init__(self, values: Dict[str, Any]) -> None:
+        self.geometry = values["geometry"]
+
+        # Compute properties
+        self.ybar = sum(w * h * (y + h / 2) for x, y, w, h in self.geometry) / sum(w * h for _, _, w, h in self.geometry)
+        # Parallel axis theorem, with I of each piece being bh^3/12
+        self.i = sum(w * h ** 3 / 12 + (y + h / 2 - self.ybar) ** 2 for x, y, w, h in self.geometry)
+    
+    def visualize(self, ax) -> None:
+        """
+        Draw the cross section onto a matplotlib plot to visualize it.
+        """
+        for x, y, w, h in self.geometry:
+            ax.add_patch(patches.Rectangle((x, y), w, h, linewidth=1, edgecolor="b", facecolor="none"))
+        ax.set_xlim(min(x for x, _, _, _ in self.geometry) - 10, max(x + w for x, _, w, _ in self.geometry) + 10)
+        ax.set_ylim(min(y for _, y, _, _ in self.geometry) - 10, max(y + h for _, y, _, h in self.geometry) + 10)
+        ax.set_aspect("equal")
 
 def load_train(geo: Geometry, dist: float) -> Forces:
     """
@@ -91,7 +113,10 @@ def make_bmd(geo: Geometry, sfd: np.ndarray) -> np.ndarray:
 
 if __name__ == "__main__":
     geo = Geometry.from_yaml("design0.yaml")
-    forces = reaction_forces(geo, load_train(geo, 960))
-    from matplotlib import pyplot as plt
-    plt.plot(np.arange(0, geo.bridge_length + 1, 1), make_bmd(geo, make_sfd(geo, forces)))
+    #forces = reaction_forces(geo, load_train(geo, 960))
+
+    #plt.plot(np.arange(0, geo.bridge_length + 1, 1), make_bmd(geo, make_sfd(geo, forces)))
+    #plt.show()
+
+    geo.bridge_cross_sections[0].visualize(plt.gca())
     plt.show()
